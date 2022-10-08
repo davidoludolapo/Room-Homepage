cc2628a4b93ffcb3c81aebbc5a14850e3f59f604
davidoludolapo <davidoluwadolapo@gmail.com>
2022-10-08 09:44:34 +0100

    room ui commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import About from "./components/About";
+import Header from "./components/Header";
+import Showcase from "./components/Showcase";
+
+
+function App() {
+  return (
+    <>
+     <Header/>
+     <Showcase/>
+     <About/>
+    </>
+  );
+}
+
+export default App;
